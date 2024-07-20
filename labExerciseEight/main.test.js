3397d59a58d34a88262e486bc6f00913f9566c62
azimy-hamid <hamidazimy875@gmail.com>
2024-07-20 10:19:17 +0800

    Modified the resume website

--- labExerciseEight/main.test.js
+++ labExerciseEight/main.test.js
@@ -1,29 +1,30 @@
-const rotateDiv = require("./main.js");
+const mainJs = require("./main.js");
 
 describe("rotateDiv", () => {
   let element;
 
   beforeEach(() => {
-    // Create a mock element
     element = document.createElement("div");
     element.style.transform = "";
+
+    // element = jest.spyOn(document, 'createElement').mockReturnValue(element);
   });
 
   test("should rotate the element by 360 degrees on first call", () => {
-    rotateDiv(element);
+    mainJs.rotateDiv(element);
     expect(element.style.transform).toBe("rotate(360deg)");
   });
 
   test("should rotate the element by 720 degrees on second call", () => {
-    rotateDiv(element);
-    rotateDiv(element);
+    mainJs.rotateDiv(element);
+    mainJs.rotateDiv(element);
     expect(element.style.transform).toBe("rotate(720deg)");
   });
 
   test("should rotate the element by 1080 degrees on third call", () => {
-    rotateDiv(element);
-    rotateDiv(element);
-    rotateDiv(element);
+    mainJs.rotateDiv(element);
+    mainJs.rotateDiv(element);
+    mainJs.rotateDiv(element);
     expect(element.style.transform).toBe("rotate(1080deg)");
   });
 });
